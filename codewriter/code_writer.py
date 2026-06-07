094c9f6f988bf50f7486c94f98d4b77a246a47ad
Marcos Antonio <marcos.branco@brpolen.com.br>
2026-06-07 16:55:50 -0300

    feat(codewriter): implementa comparações eq, gt e lt com labels únicos

--- codewriter/code_writer.py
+++ codewriter/code_writer.py
@@ -5,6 +5,7 @@ class CodeWriter:
     def __init__(self, filename: str):
         self._file = open(filename, "w")
         self._base_name = os.path.splitext(os.path.basename(filename))[0]
+        self._label_counter = 0
 
     def _write(self, *lines: str) -> None:
         for line in lines:
@@ -29,6 +30,30 @@ class CodeWriter:
             self._write("@SP", "AM=M-1", "D=M", "A=A-1", "M=M-D")
         elif command == "neg":
             self._write("@SP", "A=M-1", "M=-M")
+        elif command in ("eq", "gt", "lt"):
+            jump = {"eq": "JEQ", "gt": "JGT", "lt": "JLT"}[command]
+            label = command.upper()
+            n = self._label_counter
+            self._label_counter += 1
+            self._write(
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "D=M-D",
+                f"@{label}_TRUE_{n}",
+                f"D;{jump}",
+                "@SP",
+                "A=M-1",
+                "M=0",
+                f"@{label}_END_{n}",
+                "0;JMP",
+                f"({label}_TRUE_{n})",
+                "@SP",
+                "A=M-1",
+                "M=-1",
+                f"({label}_END_{n})",
+            )
 
     def writePop(self, segment: str, index: int) -> None:
         pass
